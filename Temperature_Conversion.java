4303baaaee31c12994478ff7a20ae10c16f3f179
Praveen240503 <96769580+Praveen240503@users.noreply.github.com>
2022-09-09 11:18:53 +0530

    import java.util.Scanner; class simpleinterest { 	public static void main(String args[]) 	{ 		double a,r; 		Scanner sc=new Scanner(System.in); 		a=sc.nextInt();
     	    r=32+(a*9/5);
    		
    		System.out.format("%.2f",r);
    		
    		sc.close();
    	}
    
    }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class simpleinterest
+{
+	public static void main(String args[])
+	{
+		double a,r;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+ 	
+ 	    r=32+(a*9/5);
+		
+		System.out.format("%.2f",r);
+		
+		sc.close();
+	}
+
+}
