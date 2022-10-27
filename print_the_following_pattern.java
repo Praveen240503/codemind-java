72321090184324cff9d571312d445542581f5d86
Praveen240503 <96769580+Praveen240503@users.noreply.github.com>
2022-10-27 10:34:20 +0530

    import java.util.*; class Solutio {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=n+1;i>1;i--)         {             for(int j=1;j<i;j++)             {                 System.out.print((char)(i+63)+" ");             }             System.out.println();         }     } }

---
+++ print_the_following_pattern.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solutio {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=n+1;i>1;i--)
+        {
+            for(int j=1;j<i;j++)
+            {
+                System.out.print((char)(i+63)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
